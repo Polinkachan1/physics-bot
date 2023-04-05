9053f6a8d1ec1a4e68a3e4f0d80d74851fdbb3ac
Polina <polinaandr123@yandex.com>
2023-04-05 18:04:35 +0300

    init db

---
+++ data/formula.py
@@ -0,0 +1,13 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Formula(SqlAlchemyBase):
+    __tablename__ = 'formulas'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    topic = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    formula_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    formula = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    explanation = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    details = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
