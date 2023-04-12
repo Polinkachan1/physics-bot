9d8d2a873063e7572a03946e44993cc3f97ff0c6
Huawei <kiseleva131415@mail.ru>
2023-04-13 00:04:38 +0300

    fix migrations

--- add_formula.py
+++ add_formula.py
@@ -1,4 +1,3 @@
-from sqlachemy.orm import select
 from data.formula import Formula
 from data.db_session import global_init, create_session
 
@@ -6,7 +5,7 @@ global_init('db/formulas.sqlite')
 session = create_session()
 
 formula1 = Formula()
-formula1.grade = 9
+formula1.year = 9
 formula1.topic = 'Законы взаимодействия и движения тел'
 formula1.formula_name = 'Формула скорости'
 formula1.formula = 'v = s/t'
@@ -15,35 +14,35 @@ formula1.explanation = 'Скорость – физическая величин
 formula1.details = 'v – скорость [м/с], s – путь [м], t – время [c]'
 
 formula2 = Formula()
-formula2.grade = 9
+formula2.year = 9
 formula2.topic = 'Законы взаимодействия и движения тел'
 formula2.formula_name = 'Вычисление перемещения'
 formula2.formula = 'АВ2 = АС2 + ВС2'
 formula2.explanation = 'Перемещение – вектор, соединяющий начальную точку движения тела с его конечной точкой.'
 
 formula3 = Formula()
-formula3.grade = 9
+formula3.year = 9
 formula3.topic = 'Законы взаимодействия и движения тел'
 formula3.formula_name = 'Проекция вектора перемещения'
 formula3.formula = 'Sx = x2 – x1'
 formula3.details = 'x1 – начальная координата [м], x2 – конечная координата [м], Sx – перемещение [м]'
 
 formula4 = Formula()
-formula4.grade = 9
+formula4.year = 9
 formula4.topic = 'Законы взаимодействия и движения тел'
 formula4.formula_name = 'Уравнение движения	'
 formula4.formula = 'x = x0 + vxt'
 formula4.details = 'x0 – начальная координата [м], x – конечная координата [м], v – скорость [м/с], t – время [c]'
 
 formula5 = Formula()
-formula5.grade = 9
+formula5.year = 9
 formula5.topic = 'Законы взаимодействия и движения тел'
 formula5.formula_name = 'Уравнение скорости'
 formula5.formula = 'v = v0⃗+ at'
 formula5.details = 'v – конечная скорость [м/с], v0 – начальная скорость [м/с], a – ускорение [м/с2], t – время [c]'
 
 formula6 = Formula()
-formula6.grade = 9
+formula6.year = 9
 formula6.topic = 'Законы взаимодействия и движения тел'
 formula6.formula_name = 'Уравнение Галилея'
 formula6.formula = 'S = v0t + at2/2'
@@ -52,7 +51,7 @@ formula6.details = 'S – перемещение [м], v – конечная с
                    ' a – ускорение [м/с2],t – время, [c]'
 
 formula7 = Formula()
-formula7.grade = 9
+formula7.year = 9
 formula7.topic = 'Законы взаимодействия и движения тел'
 formula7.formula_name = 'Первый закон Ньютона'
 formula7.explanation = '	Если на тело не действуют никакие тела либо их действие скомпенсировано, то это тело будет' \
