88f720fd42e8bbdf36c972130d82fc180942c4bb
Polinkachan1 <118909072+Polinkachan1@users.noreply.github.com>
2023-04-12 19:00:20 +0300

    Add files via upload
    fix bags

--- main.py
+++ main.py
@@ -12,31 +12,30 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     # start menu
-    bot.send_message(message.chat.id, "Привет. Выбери, что хочешь делать?")
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     grade = types.KeyboardButton("Выбор класса")
     search_formula = types.KeyboardButton("Найти формулу")
     search_information = types.KeyboardButton("Найти тему")
     get_all = types.KeyboardButton("Все формулы")
-    add_formula = types.KeyboardButton("Добавить формулу")
-    markup.add(grade, search_formula, search_information, get_all, add_formula)
+    add_form = types.KeyboardButton("Добавить формулу")
+    markup.add(grade, search_formula, search_information, get_all, add_form)
+    bot.send_message(message.chat.id, "Привет. Выбери, что хочешь делать?", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def replies(message):
-    # choice of grade
-    if message.text == "Выбор класса":
+    # choice of grade(работает)
+    if message.text == "Выбор класса" or message.text == "Поменять класс":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         grade_9 = types.KeyboardButton("9-й класс")
         grade_10 = types.KeyboardButton("10-й класс")
         grade_11 = types.KeyboardButton("11-й класс")
         markup.add(grade_9, grade_10, grade_11)
-        bot.send_message(message.chat.id, 'В каком вы классе?')
+        bot.send_message(message.chat.id, 'В каком вы классе?', reply_markup=markup)
     # find formula
     elif message.text == 'Найти формулу':
         bot.send_message(message.chat.id, "Чтобы найти формулу, напишите найди формулу Название формулы")
-        print_needen_formula()
-        # сделано(вроде)
+
     # find topic
     elif message.text == 'Найти тему':
         bot.send_message(message.chat.id, "Чтобы найти тему, напишите - найди тему Название тему ")
@@ -48,147 +47,140 @@ def replies(message):
     # output - all formulas from db
     elif message.text == "Все формулы":
         bot.send_message(message.chat.id, 'Готовлю для вас список всех формул....Пожалуйста ожидайте.')
-        # сделано(вроде)
+        bot.send_message(message.chat.id, get_all_formulas())
 
-    # 9 grade
-    elif message.text == '9-й класс':
+    # 9 grade(working)
+    elif message.text == '9-й класс' or message.text == '9':
         global needen_grade
         needen_grade = 9
-        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 9-го класса. Что вы хотите сделать?')
+        print(needen_grade)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         search_formula = types.KeyboardButton("Найти формулу")
         search_information = types.KeyboardButton("Найти тему")
         get_all = types.KeyboardButton("Все формулы")
-        add_formula = types.KeyboardButton("Добавить формулу")
-        markup.add(search_formula, search_information, get_all, add_formula)
+        add_form = types.KeyboardButton("Добавить формулу")
+        change_class = types.KeyboardButton("Поменять класс")
+        markup.add(search_formula, search_information, get_all, add_form, change_class)
+        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 9-го класса. Что вы хотите сделать?', reply_markup=markup)
 
-    # 10 grade
-    elif message.text == '10-й класс':
+    # 10 grade(working)
+    elif message.text == '10-й класс' or message.text == '10':
         needen_grade = 10
-        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 10-го класса. Что вы хотите сделать?')
+        print(needen_grade)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        search_formula = types.KeyboardButton("Найти формулу")
-        search_information = types.KeyboardButton("Найти тему")
-        get_all = types.KeyboardButton("Все формулы")
-        add_formula = types.KeyboardButton("Добавить формулу")
-        markup.add(search_formula, search_information, get_all, add_formula)
+        markup.add(
+            types.KeyboardButton("Найти формулу"),
+            types.KeyboardButton("Найти тему"),
+            types.KeyboardButton("Все формулы"),
+            types.KeyboardButton("Добавить формулу"),
+            types.KeyboardButton("Поменять класс"),
+        )
+        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 10-го класса. Что вы хотите сделать?', reply_markup=markup)
 
-    # 11 grade
-    elif message.text == '11-й класс':
+    # 11 grade(working)
+    elif message.text == '11-й класс' or message.text == '11':
         needen_grade = 11
-        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 11-го класса. Что вы хотите сделать?')
+        print(needen_grade)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         search_formula = types.KeyboardButton("Найти формулу")
         search_information = types.KeyboardButton("Найти тему")
         get_all = types.KeyboardButton("Все формулы")
-        add_formula = types.KeyboardButton("Добавить формулу")
-        markup.add(search_formula, search_information, get_all, add_formula)
+        add_form = types.KeyboardButton("Добавить формулу")
+        change_class = types.KeyboardButton("Поменять класс")
+        markup.add(search_formula, search_information, get_all, add_form, change_class)
+        bot.send_message(message.chat.id, 'Теперь вам доступна программа только 11-го класса. Что вы хотите сделать?', reply_markup=markup)
 
     # search by name of formula
     elif message.text[:13].lower().startswith('найди формулу'):
         global what_to_find
         what_to_find = message.text[14:]
         bot.send_message(message.chat.id, 'Ищу формулу по вашему запросу....Пожалуйста ожидайте.')
-        print_all_formulas_to_table()
+        bot.send_message(message.chat.id, get_needen_formula(what_to_find))
 
     # search by topic
-    elif message.text[:10].lower().startswith('найди тему'):
+    elif message.text[:10].lower().startswith('найди тему: '):
         global topic_to_find
         topic_to_find = message.text[11:]
+        print(topic_to_find)
+        bot.send_message(message.chat.id, get_needen_topic(topic_to_find))
         bot.send_message(message.chat.id, 'Ищу формулы по выбранной теме....Пожалуйста ожидайте.')
 
     # adding new formula with needen params
     # get name_of_formula
     elif message.text[:14].lower().startswith('новая формула:'):
-        global name_f
         name_f = message.text[15:]
+        print(name_f)
+        add_formula(current_formula=name_f)
         bot.send_message(message.chat.id, 'Так с названием разобрались, продолжим. Введите формулу в формате - формула: .....')
     # get formula
     elif message.text[:8].lower().startswith('формула:'):
-        global new_formula
         new_formula = message.text[9:]
+        print(new_formula)
+        add_formula(name_of_formula=new_formula)
         bot.send_message(message.chat.id, 'Так формула есть, продолжим. Введите тему в формате - тема: .....')
     # get theme
     elif message.text[:5].lower().startswith('тема:'):
-        global new_topic
-        new_topic = message.text[5:]
-        bot.send_message(message.chat.id, 'С темой определились. Введите пояснения к формуле: .....')
+        new_topic = message.text[6:]
+        print(new_topic)
+        add_formula(topic_of_form=new_topic)
+        bot.send_message(message.chat.id, 'С темой определились. Введите пояснения к формуле - пояснения: .....')
     elif message.text[:10].lower().startswith('пояснения:'):
-        global explaining
         explaining = message.text[11:]
-        add_formula(new_topic, needen_grade, name_f, new_formula, explaining, details='отсутствуют')
+        print(explaining)
+        add_formula(explanation=explaining, details='отсутствуют')
         bot.send_message(message.chat.id, 'Пояснение принято. Формула будет добавлена в каталог в ближайшее время.')
 
 
-def get_all_formulas(chat_id):  # получение всех формул за все классы
+def get_all_formulas():  # получение всех формул за все классы(работает)
     session = create_session()
-    all_formulas = session.query(Formula).all()
-    row = [(note.topic, note.formula_name, note.formula, note.explanation, note.details) for note in all_formulas]
-    return row
+    notes = session.query(Formula).all()
+    if len(notes) == 0:
+         return 'Это были все формулы, которые у нас есть.'
+    return '\n'.join(f'{note.topic}, {note.formula_name}, {note.formula}, {note.explanation}, {note.details}' for note in notes)
 
 
-# message all formulas
-def print_all_formulas_to_table(message):
-    table = PrettyTable()
-    table.field_names = ['Тема', 'Название формулы', 'Формула', 'Обьяснение', 'Детали']
-    all_formulas = get_all_formulas(message.chat.id)
-    for note in enumerate(all_formulas):
-        table.add_row(note)
-    bot.send_message(message.chat.id, table)
+#message all formulas
+#def print_all_formulas_to_table():
+    #table = PrettyTable()
+    #table.field_names = ['Тема', 'Название формулы', 'Формула', 'Обьяснение', 'Детали']
+    #all_formulas = get_all_formulas()
+    #for note in enumerate(all_formulas):
+        #table.add_row(note)
+    #bot.send_message(all_formulas)
 
 
-# get one formula
-def get_needen_formula(chat_id):
+# get one formula(работает)
+def get_needen_formula(what_to_find):
     session = create_session()
     if needen_grade:
-        formula = session.query(Formula).filter(Formula.grade == needen_grade, Formula.formula_name == what_to_find)
-        result = [(formula.formula_name, formula.formula, formula.explanation) for form in formula]
+        for formula in session.query(Formula).filter(Formula.grade == needen_grade, Formula.formula_name == what_to_find):
+            return (f' {formula.formula_name}, {formula.formula}, {formula.explanation}')
     else:
-        formula = session.query(Formula).filter(Formula.formula_name == what_to_find)
-        result = [(formula.formula_name, formula.formula, formula.explanation) for form in formula]
-    return result
+        for formula in session.query(Formula).filter(Formula.formula_name == what_to_find):
+            return (f' {formula.formula_name}, {formula.formula}, {formula.explanation}')
 
-
-# print one formula
-def print_needen_formula(message):
-    search_formula = get_needen_formula(message.chat.id)
-    table = PrettyTable()
-    table.field_names = ['Название формулы', 'Формула', 'Обьяснение']
-
-    bot.send_message(message.chat.id, table)
 
 
 # find topic
-def get_needen_topic(chat_id):
+def get_needen_topic(topic_to_find):
     session = create_session()
+    needen_grade = 9
     if needen_grade:
-        formula = session.query(Formula).filter(Formula.grade == needen_grade, Formula.topic == topic_to_find)
-        result = [(formula.formula_name, formula.formula, formula.explanation) for form in formula]
+        notes = session.query(Formula).filter(Formula.grade == needen_grade, Formula.formula_name == topic_to_find)
+        return '\n'.join(f'{note.topic}, {note.formula_name}, {note.formula}, {note.explanation}, {note.details}' for note in notes)
+
     else:
-        formula = session.query(Formula).filter(Formula.formula_name == topic_to_find)
-        result = [(formula.formula_name, formula.formula, formula.explanation) for form in formula]
-    return result
+        notes = session.query(Formula).filter(Formula.formula_name == topic_to_find)
+        return '\n'.join(
+            f'{note.topic}, {note.formula_name}, {note.formula}, {note.explanation}, {note.details}' for note in notes)
 
 
-# print topic
-def print_needen_topic(message):
-    search_formula = get_needen_formula(message.chat.id)
-    table = PrettyTable()
-    table.field_names = ['Тема', 'Название формулы', 'Формула', 'Обьяснение']
-
-    bot.send_message(message.chat.id, table)
-
-
-def add_formula(topic_of_form, needen_grade, name_of_formula, current_formula, explanation, details):
+def add_formula(name_of_formula):
     session = create_session()
     formula = Formula(
-        topic=topic_of_form,
-        grade=needen_grade,
         formula_name=name_of_formula,
-        formula=current_formula,
-        explanation=explanation,
-        details=details,
     )
+    print(formula)
     session.add(formula)
     session.commit()
 
